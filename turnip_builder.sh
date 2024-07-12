fe05fa2c2e763e3414a6dadcdfb8f3ec28d7ac12
! D _ W O R D ! <152041395+PurpleVoyager401@users.noreply.github.com>
2024-07-12 14:51:13 +0000

    turnip_builder: Change sdkver to 29

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -7,7 +7,7 @@ deps="meson ninja patchelf unzip curl pip flex bison zip"
 workdir="$(pwd)/turnip_workdir"
 magiskdir="$workdir/turnip_module"
 ndkver="android-ndk-r26c"
-sdkver="31"
+sdkver="29"
 mesasrc="https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.zip"
 clear
 
